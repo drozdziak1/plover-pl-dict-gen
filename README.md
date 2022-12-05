424a7a850fbec314ad9ef908493a056b48d928b9
Stan Drozd <drozdziak1@gmail.com>
2022-12-05 23:08:42 +0100

    Add a nix build for the rust package

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # plover-pl-dict-gen
-This is a quick and dirty Rust crate to process the `odm.txt` Polish words dataset into a usable [Plover](http://www.openstenoproject.org/plover/) dictionary. 
+This is a quick and dirty toolkit to process the `odm.txt` Polish words dataset into a usable [Plover](http://www.openstenoproject.org/plover/) dictionary. 
 
 # Credits
 * `odm.txt` - Licensed under CC-BY 4.0, created by [SJP.PL](https://sjp.pl/sl/odmiany/)
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "The Polish machine stenography dictionary generator for Plover";
+
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = github:NixOS/nixpkgs/release-22.11;
+    rust-overlay.url = github:oxalica/rust-overlay;
+    cargo2nix = {
+      url = github:cargo2nix/cargo2nix;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+  };
+
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, cargo2nix }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      rustVersion = "1.65.0";
+      pkgs = import nixpkgs {
+        overlays = [ rust-overlay.overlays.default cargo2nix.overlays.default ];
+        inherit system;
+      };
+      rustPkgs = pkgs.rustBuilder.makePackageSet {
+        inherit rustVersion;
+        packageFun = import ./rust/Cargo.nix;
+      };
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rust-bin.stable.${rustVersion}.default
+          cargo2nix.outputs.packages.${system}.cargo2nix
+        ];
+      };
+      packages = {
+        plover-pl-dict-gen-rs = (rustPkgs.workspace.plover-pl-dict-gen-rs { }).bin;
+        default = self.packages.${system}.plover-pl-dict-gen-rs;
+      };
+    });
+}
