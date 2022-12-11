baae20f376ab6263d9cacc2419d2c6cffa48c17a
Stan Drozd <drozdziak1@gmail.com>
2022-12-11 01:09:36 +0100

    morfeusz2.nix: Add the polimorf dictionary

--- nix/packages/morfeusz2.nix
+++ nix/packages/morfeusz2.nix
@@ -2,15 +2,31 @@
 let
   version = "20221204";
   python3WithDeps = python3.withPackages (ps: with ps; [ pyparsing ]);
+  polimorf-dict = stdenv.mkDerivation {
+    pname = "morfeusz2-polimorf-dict"; inherit version;
+    src = fetchurl {
+      url = "http://download.sgjp.pl/morfeusz/${version}/polimorf-${version}.tab.gz";
+      hash = "sha256-ujoL85ypjuxmqRB37GaKl5PDD4+AYQbnfVSfHg7g9z8=";
+    };
+    setSourceRoot = "sourceRoot=`pwd`";
+    unpackPhase = ''
+      cp $src .
+      gunzip *.tab.gz
+    '';
+    installPhase = ''
+      mkdir -p $out
+      mv *.tab $out/polimorf.tab
+    '';
+  };
 in
 stdenv.mkDerivation {
   pname = "morfeusz2";
   src = fetchurl {
     url = "http://download.sgjp.pl/morfeusz/${version}/morfeusz-src-${version}.tar.gz";
-    sha256 = "sha256-j9b+XZC6OIfD0B2vLxJRFhpMjtgR1YWDNmpGtF7yfuU=";
+    hash = "sha256-j9b+XZC6OIfD0B2vLxJRFhpMjtgR1YWDNmpGtF7yfuU=";
   };
-  nativeBuildInputs = [ cmake openjdk cppunit swig ];
-  buildInputs = [ python3WithDeps ];
+  cmakeFlags = [ "-DINPUT_DICTIONARIES=${polimorf-dict.out}/polimorf.tab" "-DDEFAULT_DICT_NAME=polimorf" "-DEMBEDDED_DEFAULT_DICT=1" ];
+  nativeBuildInputs = [ cmake openjdk cppunit swig python3WithDeps ];
   inherit version;
   JAVA_TOOL_OPTIONS = "-Dfile.encoding=UTF8";
 }
